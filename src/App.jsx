7dbe3b77178f82da7f9b0c7131582b0781e67a7e
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-04-14 12:01:00 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import EOQPage from "./pages/EOQPage";
 import Analytics from "./pages/Analytics";
